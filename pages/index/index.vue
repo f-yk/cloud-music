4f04d5ecb8fa6f5a48f721c012b376784e499e06
f-yk <457705306@qq.com>
2023-05-04 17:09:19 +0800

    uview

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -37,6 +37,13 @@
 				</view>
 			</scroll-view>
 		</view>
+		<u-tabbar :value="value6" @change="name => value6 = name" :fixed="true" :placeholder="true"
+			:safeAreaInsetBottom="true">
+			<u-tabbar-item text="首页" icon="home"></u-tabbar-item>
+			<u-tabbar-item text="放映厅" icon="photo"></u-tabbar-item>
+			<u-tabbar-item text="直播" icon="play-right"></u-tabbar-item>
+			<u-tabbar-item text="我的" icon="account"></u-tabbar-item>
+		</u-tabbar>
 	</view>
 </template>
 
